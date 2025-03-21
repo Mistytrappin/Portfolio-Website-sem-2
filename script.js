bbc6ae3411daacdc4ba4bf5401dcacc9a9583d81
Maks Mistewicz <mistewicz.maks@gmail.com>
2025-03-21 16:42:49 +0100

    First version of the layout that I coded.

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll(".scroll-link");
+
+    links.forEach(link => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute("href").substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                window.scrollTo({
+                    top: targetSection.offsetTop,
+                    behavior: "smooth"
+                });
+            }
+        });
+    });
+});
