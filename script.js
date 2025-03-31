0af02550401f81e7c19a2327737bb054f87f40c2
Maks Mistewicz <mistewicz.maks@gmail.com>
2025-03-31 17:55:36 +0200

    Creating drop down menu

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const links = document.querySelectorAll(".scroll-link");
-  
-    links.forEach(link => {
+  const links = document.querySelectorAll(".scroll-link");
+
+  links.forEach(link => {
       link.addEventListener("click", (e) => {
-        e.preventDefault();
-        const targetId = link.getAttribute("href").substring(1);
-        const targetSection = document.getElementById(targetId);
-  
-        if (targetSection) {
-          window.scrollTo({
-            top: targetSection.offsetTop,
-            behavior: "smooth"
-          });
-        }
+          e.preventDefault();
+          const targetId = link.getAttribute("href").substring(1);
+          const targetSection = document.getElementById(targetId);
+
+          if (targetSection) {
+              window.scrollTo({
+                  top: targetSection.offsetTop,
+                  behavior: "smooth"
+              });
+          }
+
+    
+          const menu = document.getElementById("dropdownMenu");
+          if (menu) {
+              menu.style.display = "none";
+          }
       });
-    });
-  
-   
+  });
+});
+
+function toggleMenu() {
+  const menu = document.getElementById("dropdownMenu");
+  if (menu.style.display === "flex") {
+      menu.style.display = "none";
+  } else {
+      menu.style.display = "flex";
+  }
+}
